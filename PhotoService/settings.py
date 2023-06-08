283fa190f7b6fc0736069c226772975301dc76a7
tama7000 <yu-mizuno@cosfa.co.jp>
2023-06-08 10:49:38 +0900

    7

--- PhotoService/settings.py
+++ PhotoService/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,"static"),
+    os.path.join(BASE_DIR),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
