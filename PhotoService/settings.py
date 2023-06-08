4fd295dd32f5f4424dcc667e872fc519c83d8ada
tama7000 <yu-mizuno@cosfa.co.jp>
2023-06-08 13:24:27 +0900

    14

--- PhotoService/settings.py
+++ PhotoService/settings.py
@@ -11,15 +11,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ml9g$acamw93(m4)#a(fv35r)wr48kf*zidm9j!#q7^)i1gytv'
+# SECRET_KEY = 'django-insecure-ml9g$acamw93(m4)#a(fv35r)wr48kf*zidm9j!#q7^)i1gytv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com', '{|kumachi|}.pythonanywhere.com']
 
-
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 # Application definition
+
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = get_random_secret_key() 
 
 INSTALLED_APPS = [
     'django.contrib.admin',
