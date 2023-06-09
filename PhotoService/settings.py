fb1e70db7aee3cec0deb4816649f61d32ae67e09
tama7000 <yu-mizuno@cosfa.co.jp>
2023-06-09 10:53:33 +0900

    23

--- PhotoService/settings.py
+++ PhotoService/settings.py
@@ -112,7 +112,9 @@ STATIC_URL = '/static/'
 #STATICFILES_DIRS = (
     #os.path.join(BASE_DIR,"static"),
 #)
-STATIC_ROOT = "\kumastaguramu\PhotoService\PhotoService\static"
+#STATIC_ROOT = "\kumastaguramu\PhotoService\PhotoService\static"
+#STATIC_ROOT = "\kumastaguramu\PhotoService\app\static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
