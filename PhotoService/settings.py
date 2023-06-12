8610c1c8735f07d81aff2881f31233484edff72a
tama7000 <yu-mizuno@cosfa.co.jp>
2023-06-12 16:35:51 +0900

    33

--- PhotoService/settings.py
+++ PhotoService/settings.py
@@ -20,8 +20,8 @@ from django.core.management.utils import get_random_secret_key
 SECRET_KEY = get_random_secret_key()  
 
 
-DEBUG = True
-#DEBUG = False
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
