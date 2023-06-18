83bf0a8a06867b52422240182ede3f16762e45e4
tama7000 <yu-mizuno@cosfa.co.jp>
2023-06-18 16:08:00 +0900

    58

--- app/static/css/script.js
+++ app/static/css/script.js
@@ -1,20 +1,20 @@
 console.clear()
 
-// load 27 random images from picsum
+
 var fragment = document.createDocumentFragment()
 for (var i = 0; i < 27; i++) {
-   // create photo div
+   
    var photo = document.createElement("div")
-   // add data-scroll attribute so ScrollOut will target these elements
+   
    photo.setAttribute('data-scroll', '')
-   // load the photo as a background image so it will fit nicely
+   
    photo.style.backgroundImage = 'url(https://picsum.photos/400/300/?random&hash=' + i + ')'
    fragment.appendChild(photo)
 }
-// append document fragments to container
+
 document.querySelector(".container").appendChild(fragment)
 
-// call scrollout with variables enabled
+
 ScrollOut({ 
    threshhold: 0.5, 
    cssProps: { 
