25aa0484704c3405486c203cf2fd91522bd3d231
tama7000 <yu-mizuno@cosfa.co.jp>
2023-06-18 16:09:59 +0900

    59

--- PhotoService/settings.py
+++ PhotoService/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'django_cleanup',
-     'whitenoise.middleware.WhiteNoiseMiddleware',    
+    'whitenoise.middleware.WhiteNoiseMiddleware',    
 ]
 
 MIDDLEWARE = [
